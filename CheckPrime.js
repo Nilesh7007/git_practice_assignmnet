b875ef632a4fd4f6106a1689873d84930a78e18b
KamleshRahane <kamleshrahane999@gmail.com>
2023-01-17 18:28:58 +0530

    m2 task

--- CheckPrime.js
+++ CheckPrime.js
@@ -1,8 +1,9 @@
-let num = 7;
+let num = 10;
 let count = 0;
 for(let i=2; i<=num/2; i++){
-if(num%i==0){
+if(num%i!=0){
 count++;
 }
 }
-(count==0) ? console.log(num,"prime") : console.log(num, "not prime")
+(coghitunt==0) ? console.log(num,"prime") : console.log(num, "not prime")
+//m2 task done
