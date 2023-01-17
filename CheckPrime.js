40b9870b11f30f0120dbd106e1bf1d638d157e0c
KamleshRahane <kamleshrahane999@gmail.com>
2023-01-17 17:00:10 +0530

    m2-code-fix

--- CheckPrime.js
+++ CheckPrime.js
@@ -1,7 +1,8 @@
 let num = 7;
 let count = 0;
-for(let i=0; i<=num; i++){
+for(let i=2; i<=num/2; i++){
 if(num%i==0){
 count++;
 }
-(count==2) ? console.log(num,"prime") : console.log(num, "not prime")
+}
+(count==0) ? console.log(num,"prime") : console.log(num, "not prime")
